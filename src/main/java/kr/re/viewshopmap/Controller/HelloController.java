e14fbf0606a5a44862a26e30ab35e6782ddb7e7f
jingug1004 <jingug1004@gmail.com>
2019-05-12 21:15:21 +0900

    190512 (토) [Front] Java & JSP 에서 [Back] Java REST API 호출 성공 http://localhost:8071/hello

---
+++ src/main/java/kr/re/viewshopmap/Controller/HelloController.java
@@ -0,0 +1,54 @@
+package kr.re.viewshopmap.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by A on 2019-05-04 오전 11:32
+ * viewshopmap / kr.re.viewshopmap.Controller
+ * No pain, No gain!
+ * What :
+ * Why :
+ * How :
+ * << 개정이력(Modification Information) >>
+ * 수정일         수정자          수정내용
+ * -------       --------       ---------------------------
+ * 2018/04/01     김진국          최초 생성
+ * 2017/05/27     이몽룡          인증이 필요없는 URL을 패스하는 로직 추가
+ *
+ * @author 개발팀 김진국
+ * @version 1.0
+ * @see
+ * @since 2018/04/01
+ */
+
+//출처: https://jsijsi99.tistory.com/10 [삽질대마왕 이대장]
+@Controller
+public class HelloController {
+
+    // @GetMapping
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public void hello(Model model) {
+
+        String a1 = "snviesrn234";
+
+        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<String> response = restTemplate.getForEntity("http://info.sweettracker.co.kr/v2/api-docs?item_no=149480994106", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8070/ap/product/best", String.class);
+
+        String html = response.getBody();
+        System.out.println(html);
+        // [출처] Java Rest API 호출|작성자 cloba
+
+        model.addAttribute("testStrings", html);
+
+        System.out.println(model);
+//        return "hello";
+    }
+
+}
+
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,18 @@
+<%--<%@ page language="java" contentType="text/html; charset=utf-8"
+         pageEncoding="utf-8" isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>--%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Spring Boot Application with JSP</title>
+</head>
+<body>
+Hello, Spring Boot App 12345678
+${testStrings}
+</body>
+</html>
